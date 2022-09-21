c20732d246238fd48beef3e5580c35c71c9f522b
andrés <aferrarivalle@gmail.com>
2022-09-20 22:34:10 -0300

    asdasdasd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import SideBar from './components/SideBar';
-import SmallCard from './components/SmallCard';
 
 function App() {
   return (
     <div className="App">
       <SideBar />
-      <SmallCard />
     </div>
   );
 }
