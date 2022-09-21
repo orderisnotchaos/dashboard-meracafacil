14bfcea18c6c9bfe7217e231da79cb053ec8710b
andrés <aferrarivalle@gmail.com>
2022-09-20 22:18:58 -0300

    dashboard init

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import {Link} from 'react-router-dom';
+
+function SideBar(){
+    return(
+        <React.Fragment>
+            {/*<!-- Sidebar -->*/}
+            <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                {/*<!-- Sidebar - Brand -->*/}
+                <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+                    <div className="sidebar-brand-icon">
+                        <img className="w-100" src="logo-banner-transparente.png" width={33} height={33} alt="Digital House"/>
+                    </div>
+                </a>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider my-0"/>
+
+                {/*<!-- Nav Item - Dashboard -->*/}
+                <li className="nav-item active">
+                    <Link className="nav-link" to="/">
+                        <i className="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Dashboard - Merca Fácil</span></Link>
+                </li>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider"/>
+
+                {/*<!-- Heading -->*/}
+                <div className="sidebar-heading">Actions</div>
+
+                {/*<!-- Nav Item - Pages -->*/}
+                <li className="nav-item">
+                <Link className="nav-link" to="/">
+                        <i className="fas fa-fw fa-folder"></i>
+                        <span>Pages</span>
+                    </Link>
+                </li>
+
+                {/*<!-- Nav Item - Charts -->*/}
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">
+                        <i className="fas fa-fw fa-chart-area"></i>
+                        <span>Charts</span></Link>
+                </li>
+
+                {/*<!-- Nav Item - Tables -->*/}
+                <li className="nav-item nav-link">
+                <Link className="nav-link" to="/">
+                        <i className="fas fa-fw fa-table"></i>
+                        <span>Tables</span></Link>
+                </li>
+
+                {/*<!-- Divider -->*/}
+                <hr className="sidebar-divider d-none d-md-block"/>
+            </ul>
+
+        </React.Fragment>
+    )
+}
+export default SideBar;
