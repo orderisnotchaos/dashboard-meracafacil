cb82d66c9eec95708101308084c84e4fac02d9a6
andrés <aferrarivalle@gmail.com>
2022-09-23 23:17:43 -0300

    feature: added TotalPanels and some css

--- src/components/ContentWrapper/ContentWrapper.jsx
+++ src/components/ContentWrapper/ContentWrapper.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Products from '../Products/Products';
 import TopBar from '../TopBar/TopBar';
-
+import TotalPanel from '../TotalPanels/TotalPanel';
+import './ContentWrapper.css';
 function ContentWrapper(){
+    const [users, setUsers]=useState([]);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+        fetch('http://127.0.0.1:8000/api/users').then((res) =>{
+            
+            return res.json();
+        }).then(res => {
+
+            setUsers(res);
+        });
+
+        fetch('http://127.0.0.1:8000/api/products').then((res) =>{
+            
+            return res.json();
+        }).then(res => {
+
+            setProducts(res);
+        });
+    }, []);
+
     return (
         <React.Fragment>
             {/*<!-- Content Wrapper -->*/}
@@ -9,6 +33,10 @@ function ContentWrapper(){
                 {/*<!-- Main Content -->*/}
                 <div id="content">
                     <TopBar />
+                    <div className="row">
+                        <TotalPanel title= "Total de Usuarios" cuantity={users.length}/>
+                        <TotalPanel title= "Total de Productos" cuantity={products.length}/>
+                    </div>
                 </div>
             </div>    
         </React.Fragment>
